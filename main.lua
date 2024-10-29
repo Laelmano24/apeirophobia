cb2ee4f3bdec025f1a5c2e2cef2f7a164c2bddf9
Laelmano24 <brockbomba@gmail.com>
2024-10-29 12:38:21 -0300

    update main.lua

--- main.lua
+++ main.lua
@@ -9,6 +9,8 @@ Window:AddMinimizeButton({
   Corner = { CornerRadius = UDim.new(0, 6) }
 })
 local Main = Window:MakeTab({"Main", "home"})
+local Player = Window:MakeTab({Jogador.name, "user"})
+local Esp = Window:MakeTab({Mostrar.name, "eye"})
 local Credits = Window:MakeTab({"Credits", "star"})
 
 Main:AddSection("Example section ")
@@ -21,6 +23,99 @@ Main:AddButton({
     
   end
 })
+
+
+local JogadorSpeedValue = 25
+local JogadorSpeedRunService
+
+Player:AddSection({"Player speed"})
+
+local PlayerSpeed = Player:AddSlider({
+  Name = "Velocidade",
+  Min = 14,
+  Max = 25,
+  Default = 100
+})
+
+PlayerSpeed:Callback(function(Value)
+
+  JogadorSpeedValue = Value
+end)
+
+local ToggleSpeed = Player:AddToggle({
+  Name = "Enable speed",
+  Description = "",
+  Flag = "",
+  Default = false
+})
+
+ToggleSpeed:Callback(function(Value)
+  local RunService = game:GetService("RunService")
+  local Player = game.Players.LocalPlayer
+  local Humanoid = Player.Character and Player.Character:FindFirstChildOfClass("Humanoid")
+
+  if Value then
+
+    JogadorSpeedRunService = RunService.Heartbeat:Connect(function()
+      if Humanoid then
+        Humanoid.WalkSpeed = JogadorSpeedValue
+      end
+    end)
+  else
+
+    if JogadorSpeedRunService then
+      JogadorSpeedRunService:Disconnect()
+      JogadorSpeedRunService = nil
+    end
+    if Humanoid then
+      Humanoid.WalkSpeed = 16
+    end
+  end
+end)
+
+Player:AddSection({"Illuminates around you"})
+
+local PlayerFullbright = Player:AddToggle({
+  Name = "Fullbright",
+  Description = "",
+  Flag = "Toggle1",
+  Default = false
+})
+
+PlayerFullbright:Callback(function(Value)
+  if Value then
+    RaelHubFunction.dofullbright()
+  else
+    RaelHubFunction.restoreLighting()
+  end
+end)
+
+local Esp_Monsters_button = Esp:AddToggle({
+  Name = "Esp monsters",
+  Description = "",
+  Flag = "",
+  Default = false
+})
+
+Esp_Monsters_button:Callback(function(Value)
+  local Monsters -- Place the monsters
+  if Value then
+    
+    for _, Monster in ipairs(Monsters) do
+      if not Monster:FindFirstChild("RaelHubIcon") and not Monster:FindFirstChild("RaelHubDestaque") then
+         RaelHubFunction.CreateEspDistance(Monster, Color3.fromRGB(255, 102, 102), Monster.Name, true)
+         
+        Monster:FindFirstChild("RaelHubIcon").StudsOffset = Vector3.new(0, -5, 0)
+      end
+    end
+  else
+
+    local Monsters -- Place the monsters
+    for _, Monster in ipairs(Monsters) do
+      RaelHubFunction.DisableEsp(Monster)
+    end
+  end
+end)
 
 Credits:AddDiscordInvite({
   Name = "Rael Hub",
